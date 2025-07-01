bd2cc60e56afa7cdeb5acf0a9f80238c210a93dc
Leonardo Luiz <leonardo.lmf27@gmail.com>
2025-07-01 16:35:25 -0300

    feat: file entity creation

---
+++ src/main/java/br/com/oel/downlink/Model/FileEntity.java
@@ -0,0 +1,33 @@
+package br.com.oel.downlink.Model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "files")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FileEntity {
+
+    @Id
+    private String id; // id
+
+    @Column(nullable = false)
+    private String fileName; //nome do arquivo
+
+    @Column(nullable = false)
+    private String contentType; // tipo de midia
+
+
+    @Column(nullable = false)
+    private long size;
+
+    // A anotação LOB permite armazenar uma quantidade alta de dados binarios (byte)
+    @Lob
+    @Column(nullable = false)
+    private byte[] data;
+
+}

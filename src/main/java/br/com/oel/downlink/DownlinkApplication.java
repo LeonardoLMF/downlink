1138e08afeaf08a3a95ad3cd7c27b21a780bb1d9
Leonardo Luiz <leonardo.lmf27@gmail.com>
2025-07-01 14:31:29 -0300

    start

---
+++ src/main/java/br/com/oel/downlink/DownlinkApplication.java
@@ -0,0 +1,13 @@
+package br.com.oel.downlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DownlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/oel/downlink/DownlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oel.downlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
